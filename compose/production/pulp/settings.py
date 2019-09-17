9e5f3913d146f704a240e2ce9f4147454760cbb7
Alexander Saprykin <cutwatercore@gmail.com>
2019-09-17 13:49:12 +0200

    Add content path prefix parameter in pulp settings

--- compose/production/pulp/settings.py
+++ compose/production/pulp/settings.py
@@ -11,9 +11,13 @@ DATABASES = {
     }
 }
 
+CONTENT_PATH_PREFIX = '/api/automation-hub/v3/artifacts/collections/'
+
 ANSIBLE_API_HOSTNAME = ''
 
 MEDIA_ROOT = '/data/'
+
+GALAXY_API_ROOT = 'api/<str:path>/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.environ['PULP_AWS_STORAGE_BUCKET_NAME']
@@ -21,4 +25,3 @@ AWS_ACCESS_KEY_ID = os.environ['PULP_AWS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['PULP_AWS_ACCESS_KEY']
 AWS_DEFAULT_ACL = None
 
-GALAXY_API_ROOT = 'api/<str:path>/'
